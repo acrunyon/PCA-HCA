890b3e56ef5905c6d8ebb7d7a028ff024d4817b5
acrunyon <amber.c.runyon@gmail.com>
2020-11-21 12:17:23 -0700

    Creating scripts

---
+++ PCAscatterplot.R
@@ -0,0 +1,2 @@
+# PCA on WICA scatterplots
+
---
+++ Tutorials.R
@@ -0,0 +1,5 @@
+#### PCA tutorial - https://www.datacamp.com/community/tutorials/pca-analysis-r
+
+
+
+#### HCA tutorial (using mtcars) 0 http://ropatics.com/data-mining/mastering-data-analysis-with-r/Data_Analysis_-_Cluster.html
