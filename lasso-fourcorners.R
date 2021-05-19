0ff9c96e18819c1ce91ae84327d005144dda2175
acrunyon <amber.c.runyon@gmail.com>
2021-05-19 13:07:04 -0600

    Adding JHR method

--- lasso-fourcorners.R
+++ lasso-fourcorners.R
@@ -1,13 +1,13 @@
-# Convex hull
+# generate data
+set.seed(3)
 X <- as.data.frame(matrix(rnorm(40), ncol = 2))
 names(X) <- c("x","y")
-plot(X, cex = 0.5)
-hpts <- chull(X)
-hpts <- c(hpts, hpts[1])
-lines(X[hpts, ])
+X$id <- LETTERS[seq( from = 1, to = 20 )]
 
+plot(y~x,data=X, cex = 0.5,pch=19,col="grey")
+text(y~x, labels=id,data=X, cex=0.9, font=2)
 
-# 4 corners
+### 4 CORNERS
   #min bounding bo around oints
 
 lx = min(X$x)
@@ -27,7 +27,6 @@ X$WD.distance <- sqrt((X$x - wd[1])^2 + (X$y - wd[2])^2)
 X$HW.distance <- sqrt((X$x - hw[1])^2 + (X$y - hw[2])^2)
 X$HD.distance <- sqrt((X$x - hd[1])^2 + (X$y - hd[2])^2)
 
-X$id <- LETTERS[seq( from = 1, to = 20 )]
 X$id[which.min(X$WW.distance)]
 X$id[which.min(X$WD.distance)]
 X$id[which.min(X$HW.distance)]
@@ -43,6 +42,29 @@ ggplot(X, aes(x = x, y = y)) +
   geom_point(aes(x=x[which.min(HD.distance)], y=y[which.min(HD.distance)]), shape=21, size=9, stroke=2, colour="red")
 
 
+### JHR METHOD
+# Identify extreme points in each quadrant (either convex hull or Euclid dist from mean X & mean Y)
+  #Convex hull
+hpts <- chull(X[1:2]) #convex hull of points
+circle.hpts <- c(hpts, hpts[1]) #create 'circle'
+lines(X[circle.hpts, ]) #plot points
+
+X.hpts <- X[hpts,] #subset df to points
+row.names(X.hpts) <- X.hpts$id
+
+# Select combinations that are maximally divergent from one another - dissimilarity matrix
+
+n.pts = 2 #identify n points to use
+
+# dissimilarity matrix
+library(cluster)
+diss <- daisy(X.hpts[,1:2], metric = "euclidean", stand = FALSE) # allows weighting of variables as well
+diss<-as.matrix(diss)
+diss[upper.tri(diss, diag = FALSE)] <- NA 
+
+top<-tail(sort(diss), 3)
 
 
+index <- which(diss == top[1], arr.ind=TRUE)
+paste(rownames(diss)[index[1]], colnames(diss)[index[2]], sep=", ")
 
