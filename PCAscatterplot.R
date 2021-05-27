0cea98807497a95166494308cca285ef87ebcd1f
acrunyon <amber.c.runyon@gmail.com>
2021-05-27 14:49:23 -0600

    Test across multiple parks

--- PCAscatterplot.R
+++ PCAscatterplot.R
@@ -76,7 +76,6 @@ rownames(DINO.pca.x)[which.min(DINO.pca.x$PC2)]
 rownames(DINO.pca.x)[which.max(DINO.pca.x$PC2)]
 
 ############## TEST VS. LASSO APPROACH #################
- 
 
 dualscatter = ggplot(Future_Means, aes(DeltaTavg, DeltaPr*365, 
                                        xmin=quantile(Future_Means$DeltaTavg,0.25), 
@@ -174,3 +173,104 @@ rownames(YELL.pca.x)[which.max(YELL.pca.x$PC2)]
 
 #Median of Pc2?
 rownames(YELL.pca.x)[with(YELL.pca.x, which.min(abs(PC2 - mean(PC2))))]
+
+
+
+
+######### TEST PCA vs 4-CORNERS ACROSS SFIP PARKS #####################
+rm(list=ls())
+dataDir <- "C:/Users/achildress/Documents/RSS/"
+
+pnt.list<-list.files(path=dataDir, pattern=".RData")
+park<-substr(pnt.list,1,4)
+
+results<-as.data.frame(matrix(data=NA,length(park),6))
+names(results)<-c("WW","WD","HW","HD","PC1","PC2")
+results$park<-park
+
+start.time<-Sys.time()
+
+# 
+for (p in 1:length(park)){
+
+rm(list=ls()[! ls() %in% c("dataDir","pnt.list","park","results","start.time","p")])
+load(paste0(dataDir,pnt.list[p]))
+
+Baseline_all$Date = strptime(Baseline_all$Date, "%Y-%m-%d")
+Future_all$Date = strptime(Future_all$Date, "%Y-%m-%d")
+
+# # Subset Future_all to only be near future (2025-2055) and Baseline_all to only but until 2000
+ALL_HIST<-Baseline_all
+Baseline_all$Year<-format(as.Date(Baseline_all$Date, format="%Y-%m-%d"),"%Y")
+Baseline_all<-subset(Baseline_all,Year<2000)
+Baseline_all$Year<-NULL
+
+ALL_FUTURE<-Future_all  
+Future_all$yr = Future_all$Date$year + 1900
+Future_all = subset(Future_all, yr >= 2040 - (30/2) & yr <= (2040 + (30/2)))
+
+
+BaseMeanPr = mean(Baseline_all$PrecipCustom)
+BaseMeanTmx = mean(Baseline_all$TmaxCustom)
+BaseMeanTmn = mean(Baseline_all$TminCustom)
+
+####Create Future/Baseline means data tables, with averages for all four weather variables, organized by GCM
+Future_Means = data.frame(aggregate(cbind(Future_all$PrecipCustom, Future_all$TmaxCustom, Future_all$TminCustom)
+                                    ~ Future_all$GCM, Future_all, mean,na.rm=F))   # , Future_all$Wind
+names(Future_Means) = c("GCM", "PrecipCustom", "TmaxCustom", "TminCustom")    # , "Wind"
+Future_Means$TavgCustom = (Future_Means$TmaxCustom + Future_Means$TminCustom)/2
+
+Baseline_Means = data.frame(aggregate(cbind(PrecipCustom, TmaxCustom, TminCustom)~GCM, 
+                                      Baseline_all[which(Baseline_all$GCM %in% unique(Future_all$GCM)),], mean))    #  ,Baseline_all$Wind)
+names(Baseline_Means) = c("GCM", "PrecipCustom", "TmaxCustom", "TminCustom")  #  , "Wind")
+Baseline_Means$TavgCustom = (Baseline_Means$TmaxCustom + Baseline_Means$TminCustom)/2
+
+#### add delta columns in order to classify CFs
+Future_Means$DeltaPr = Future_Means$PrecipCustom - Baseline_Means$PrecipCustom
+Future_Means$DeltaTmx = Future_Means$TmaxCustom - Baseline_Means$TmaxCustom
+Future_Means$DeltaTmn = Future_Means$TminCustom - Baseline_Means$TminCustom
+Future_Means$DeltaTavg = Future_Means$TavgCustom - Baseline_Means$TavgCustom
+
+
+# 4-corners
+lx = min(Future_Means$DeltaTavg)
+ux = max(Future_Means$DeltaTavg)
+ly = min(Future_Means$DeltaPr)*365
+uy = max(Future_Means$DeltaPr)*365
+
+#convert to points
+ww = c(lx,uy)
+wd = c(lx,ly)
+hw = c(ux,uy)
+hd = c(ux,ly)
+
+#calc EuclGCMian dist of each point from corners
+Future_Means$WW.distance <- sqrt((Future_Means$DeltaTavg - ww[1])^2 + (Future_Means$DeltaPr*365 - ww[2])^2)
+Future_Means$WD.distance <- sqrt((Future_Means$DeltaTavg - wd[1])^2 + (Future_Means$DeltaPr*365 - wd[2])^2)
+Future_Means$HW.distance <- sqrt((Future_Means$DeltaTavg - hw[1])^2 + (Future_Means$DeltaPr*365 - hw[2])^2)
+Future_Means$HD.distance <- sqrt((Future_Means$DeltaTavg - hd[1])^2 + (Future_Means$DeltaPr*365 - hd[2])^2)
+
+results$WW[p] <- Future_Means$GCM[which.min(Future_Means$WW.distance)]
+results$WD[p] <-Future_Means$GCM[which.min(Future_Means$WD.distance)]
+results$HW[p] <-Future_Means$GCM[which.min(Future_Means$HW.distance)]
+results$HD[p] <-Future_Means$GCM[which.min(Future_Means$HD.distance)]
+
+# PCA
+FM2<-subset(Future_Means, select = c("GCM","DeltaTavg","DeltaPr"))
+FM2<- FM2[,-1]
+rownames(FM2) <- Future_Means[,1]
+
+PCA <- prcomp(FM2[,c(1:2)], center = TRUE,scale. = TRUE)
+
+PCA.x<-as.data.frame(PCA$x)
+
+pca1 <- c(rownames(PCA.x)[which.min(PCA.x$PC1)], rownames(PCA.x)[which.max(PCA.x$PC1)])
+pca2 <- c(rownames(PCA.x)[which.min(PCA.x$PC2)],rownames(PCA.x)[which.max(PCA.x$PC2)])
+
+results$PC1[p] <- toString(pca1)
+results$PC2[p] <- toString(pca2)
+}
+end.time<-Sys.time()
+time<-end.time-start.time
+
+write.csv(results,paste0(dataDir,"results.csv"))
